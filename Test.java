745133c733071d140df0c634d65a5ea475e00b55
GavFeng <gavfeng@uw.edu>
2023-10-14 12:14:13 -0700

    Testing Commit

--- Test.java
+++ Test.java
@@ -1,1 +1,6 @@
+class Test{
+    public int testing(int test){
+        return 0;
+    }
 
+}
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public main{
+class Main{
     public static void main(String args[]){
 
         
